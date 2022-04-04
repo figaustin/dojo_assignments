f666490128dba4ca800da0dd38bf214341385b80
Osvaldo Figueroa <figaustin@yahoo.com>
2022-04-04 17:15:00 -0400

    initial commit

---
+++ python/fundamentals/fundamentals/for_loop_basic_1.py
@@ -0,0 +1,32 @@
+for i in range(151):
+    print(i)
+
+for i in range(0,1005,5):
+    print(i)
+
+for i in range(1,100):
+    if(i % 10 == 0):
+        print('Coding Dojo')
+    elif (i % 5 == 0):
+        print('Coding')
+    else:
+        print(i)
+
+count = 0
+for i in range(0,500000):
+    if i % 2 != 0:
+        count += i
+print(count)
+
+for i in range(2018,0,-4):
+    print(i)
+
+lowNum = 2
+highNum = 18
+mult = 3
+for i in range(lowNum,highNum):
+    if i % mult == 0:
+        print(i)
+
+
+    
