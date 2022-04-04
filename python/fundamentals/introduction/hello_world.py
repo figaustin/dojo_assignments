aebb63531d602dd69f1a32bd26a510a8315339ed
Osvaldo Figueroa <figaustin@yahoo.com>
2022-04-04 01:07:01 -0400

    initial commit

--- python/fundamentals/introduction/hello_world.py
+++ python/fundamentals/introduction/hello_world.py
@@ -10,6 +10,5 @@ print('Hello ' + str(num) + '!')
 
 fave_food1 = 'sushi'
 fave_food2 = 'pizza'
-
 print('I love to eat {}'.format(fave_food1) + ' and {}'.format(fave_food2))
 print(f'I love to eat {fave_food1} and {fave_food2}')
