525043c28e93d7e9d5469df6817609d7c09b8949
Osvaldo Figueroa <figaustin@yahoo.com>
2022-04-07 18:51:45 -0400

    initial commit

---
+++ python/flask/fundamentals/playground/playground.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/play')
+def play1():
+    return render_template('index.html')
+
+@app.route('/play/<number>')
+def play2(number):
+    return render_template('playground2.html', times = int(number))
+
+@app.route('/play/<number>/<color>')
+def play3(number, color):
+    return render_template('playground3.html', number = int(number), color = color)
+if __name__ == '__main__':
+    app.run(debug=True)
