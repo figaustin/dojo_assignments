ea23d5055264aa7f87ffd8a86ffec570a80e957f
Osvaldo Figueroa <figaustin@yahoo.com>
2022-05-02 19:02:29 -0400

    initial commit

---
+++ mern/javascript/fundamentals/predict_destructuring/predict_destructuring.js
@@ -0,0 +1,14 @@
+/*
+Problem 1: Output = Tesla, Mercedes
+
+Problem 2: Output = name is not defined, Elon
+
+Problem 3: Output = 12345, undefined
+
+Problem 4: Output = false, true
+
+Problem 5: Output = value, [1,5,1,8,3,3], 1, 5
+*/
+
+
+
