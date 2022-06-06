bf9b7742623f46c0284d42db18c4c6bd480a43a1
Osvaldo Figueroa <figaustin@yahoo.com>
2022-06-06 19:33:23 -0400

    initial commit

---
+++ java/java_foundations/fundamentals/first_java_program/First.java
@@ -0,0 +1,7 @@
+public class First {
+    public static void main(String[] args){
+        System.out.println("My name is Austin");
+        System.out.println("I am 24 years old");
+        System.out.println("My hometown is Webster, Massachusetss");
+    }   
+}
