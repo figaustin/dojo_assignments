dc540c07e5dde40f7614f88df640d65c28e66768
Osvaldo Figueroa <figaustin@yahoo.com>
2022-06-11 22:09:11 -0400

    initial commit

--- java/java_spring/spring_intro/hello_human/src/main/java/com/austin/HelloHumanController.java
+++ java/java_spring/spring_intro/hello_human/src/main/java/com/austin/HelloHumanController.java
@@ -1,5 +1,21 @@
 package com.austin;
 
-public class HelloHumanController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class HelloHumanController {
+	
+	@RequestMapping("/")
+	public String hello( @RequestParam(value="name", required=false) String name) {
+		
+		if(name != null) {
+			return "Hello " + name;
+		}
+		
+		return "Hello, Human";
+	}
+	
+	
 }
