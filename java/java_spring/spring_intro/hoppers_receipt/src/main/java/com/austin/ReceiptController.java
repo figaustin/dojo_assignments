7c52dd96534fc56166067f0ca470ce183d577132
Osvaldo Figueroa <figaustin@yahoo.com>
2022-06-13 00:34:57 -0400

    initial commit

---
+++ java/java_spring/spring_intro/hoppers_receipt/src/main/java/com/austin/ReceiptController.java
@@ -0,0 +1,28 @@
+package com.austin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReceiptController {
+	
+	@RequestMapping("/")
+    public String index(Model model) {
+        
+        String name = "Grace Hopper";
+        String itemName = "Copper wire";
+        double price = 5.25;
+        String description = "Metal strips, also an illustration of nanoseconds.";
+        String vendor = "Little Things Corner Store";
+    
+    	// Your code here! Add values to the view model to be rendered
+        model.addAttribute("name", name);
+        model.addAttribute("itemName", itemName);
+        model.addAttribute("price", price);
+        model.addAttribute("desc", description);
+        model.addAttribute("vendor", vendor);
+    
+        return "demo.jsp";
+    }
+}
